9ca1dfd8c4c5aef9762c3f1678c442346f9b7b68
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-05-12 12:03:59 -0700

    Made an alphabatical order and a length order array

--- ArraySorting/main.m
+++ ArraySorting/main.m
@@ -10,8 +10,30 @@
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+
+    //This is the initial array that we are working with.
+    NSArray *initialArray = @[@"eeeeeAAbfdf", @"Everything", @"news to me", @"timepiece", @"egg", @"eggs", @"paper", @"e", @"facebook", @"painting", @"231 thurlow st", @"99 bottles"];
+    NSLog(@"Initial array:%@", initialArray);
+
+    //Sorting the initial array aphabetically with numbers first and then the alphabets.
+    NSArray *alphabeticallyArrangedArray = [initialArray sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+      return [obj1 compare:obj2 options:NSLiteralSearch];
+    }];
+      NSLog(@"Alphabetically arranged array:%@", alphabeticallyArrangedArray);
+
+    //Sort the initial array with the longest string first.
+    NSArray *longestStringArray = [initialArray sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+      if ([obj1 length] > [obj2 length]) {
+        return (NSComparisonResult)NSOrderedAscending;
+      }
+      if ([obj2 length] > [obj1 length]) {
+        return (NSComparisonResult)NSOrderedDescending;
+      }
+      return (NSComparisonResult)NSOrderedSame;
+    }];
+    NSLog(@"Array with the longest string first:%@", longestStringArray);
+
+
   }
     return 0;
 }
